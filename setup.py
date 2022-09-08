5e652d541402fbb678080a53f7b5ea85aaf857d9
lixinjian <lixinjian1217@gmail.com>
2022-09-08 14:06:19 -0400

    fix some install issue

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup,find_packages
 
+requirements=[r.strip() for r in open("requirements.txt").readlines()]
+
 setup(
    name='transphone',
-   version='1.0',
+   version='1.0.3',
    description='a multilingual g2p/p2g model',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
    url="https://github.com/xinjli/transphone",
-   packages=['transphone'],
+   packages=find_packages(),
+   install_requires=requirements,
 )
